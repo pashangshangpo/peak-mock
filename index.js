4fe91f0a0dbcfbc3ceb4e4eaff31b2d9a2b98709
爬上上坡 <pashangshangpo@gmail.com>
2018-11-23 23:46:05 +0800

    解析 mock

--- index.js
+++ index.js
@@ -7,9 +7,31 @@
 const Koa = require('koa')
 const Router = require('koa-router')
 
-module.exports = (app, mock, port = 3000) => {
+const ParseMock = mock => {
+  const Result = []
+
+  for (let key of Object.keys(mock)) {
+    let [type, path] = key.split(' ')
+
+    if (path == null) {
+      path = type
+      type = 'all'
+    }
+
+    Result.push({
+      type: type.toLowerCase(),
+      path: path
+    })
+  }
+
+  return Result
+}
+
+module.exports = (app, mock = {}, port = 3000) => {
   const App = app || new Koa()
   const router = new Router()
 
-  App.use(router.routes()).listen()
+  console.log(ParseMock(mock))
+
+  App.use(router.routes()).listen(port)
 }
